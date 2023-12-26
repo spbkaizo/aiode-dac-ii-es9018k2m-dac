267cdcaa9e232ddf33247ff4f02332729f181b0e
SimonB <simonb@kaizo.org>
2023-12-26 16:09:56 +0000

    tidy

--- rpi-es9018k2m-dac.c
+++ rpi-es9018k2m-dac.c
@@ -30,56 +30,10 @@
 
 #include "es9018k2m.h"
 
-//extern struct snd_soc_card snd_rpi_es9018k2m_dac; // Ensure this is defined elsewhere
-//extern struct snd_soc_dai_link snd_rpi_es9018k2m_dac_dai[]; // Ensure this is defined elsewhere
-
-
-
 static int snd_rpi_es9018k2m_dac_init(struct snd_soc_pcm_runtime *rtd)
 {
 	return 0;
 }
-
-/*
-static int snd_rpi_es9018k2m_dac_hw_params(struct snd_pcm_substream *substream,
-				       struct snd_pcm_hw_params *params)
-{
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	// FIXME
-	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
-
-	int bclk_ratio = snd_pcm_format_physical_width(
-			params_format(params)) * params_channels(params);
-	return snd_soc_dai_set_bclk_ratio(cpu_dai, bclk_ratio);
-}
-*/
-
-/*
-static int snd_rpi_es9018k2m_dac_hw_params(struct snd_pcm_substream *substream,
-                                           struct snd_pcm_hw_params *params)
-{
-    struct snd_soc_pcm_runtime *rtd = substream->private_data;
-    struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
-
-    int ret, bclk_ratio = snd_pcm_format_physical_width(params_format(params)) * params_channels(params);
-
-    // Access the CPU DAI from the rtd structure
-    cpu_dai = snd_soc_rtdcom_lookup(rtd, "YourPlatformName"); // Replace 'YourPlatformName' with actual platform name
-    if (!cpu_dai) {
-        pr_err("Failed to find CPU DAI\n");
-        return -EINVAL;
-    }
-
-    // Set BCLK ratio
-    ret = snd_soc_dai_set_bclk_ratio(cpu_dai, bclk_ratio);
-    if (ret < 0) {
-        pr_err("Failed to set BCLK ratio: %d\n", ret);
-        return ret;
-    }
-
-    return 0;
-}
-*/
 
 static int snd_rpi_es9018k2m_dac_hw_params(struct snd_pcm_substream *substream,
                                            struct snd_pcm_hw_params *params)
@@ -114,23 +68,6 @@ static int snd_rpi_es9018k2m_dac_hw_params(struct snd_pcm_substream *substream,
 static struct snd_soc_ops snd_rpi_es9018k2m_dac_ops = {
 	.hw_params = snd_rpi_es9018k2m_dac_hw_params,
 };
-
-/*
-static struct snd_soc_dai_link snd_rpi_es9018k2m_dac_dai[] = {
-{
-	.name		= "RPi-ES9018K2M-DAC",
-	.stream_name	= "RPi-DAC ES9018K2M HiFi",
-	.cpu_dai_name	= "bcm2708-i2s.0",
-	.codec_dai_name	= "es9018k2m-dai",
-	.platform_name	= "bcm2708-i2s.0",
-	.codec_name	= "es9018k2m-i2c.1-0048",
-	.dai_fmt	= SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
-				SND_SOC_DAIFMT_CBS_CFS,
-	.ops		= &snd_rpi_es9018k2m_dac_ops,
-	.init		= snd_rpi_es9018k2m_dac_init,
-},
-};
-*/
 
 static struct snd_soc_dai_link_component cpu_dai[] = {
     {
@@ -175,38 +112,6 @@ static struct snd_soc_card snd_rpi_es9018k2m_dac = {
 	.dai_link     = snd_rpi_es9018k2m_dac_dai,
 	.num_links    = ARRAY_SIZE(snd_rpi_es9018k2m_dac_dai),
 };
-
-/*
-static int snd_rpi_es9018k2m_dac_probe(struct platform_device *pdev)
-{
-	int ret = 0;
-
-	snd_rpi_es9018k2m_dac.dev = &pdev->dev;
-	if (pdev->dev.of_node) {
-		struct device_node *i2s_node;
-		struct snd_soc_dai_link *dai;
-		dai = &snd_rpi_es9018k2m_dac_dai[0];
-		i2s_node = of_parse_phandle(pdev->dev.of_node, "i2s-controller", 0);
-		if (i2s_node) {
-			dai->cpu_dai_name = NULL;
-			dai->cpu_of_node = i2s_node;
-			dai->platform_name = NULL;
-			dai->platform_of_node = i2s_node;
-		} else {
-			dev_err(&pdev->dev,
-			    "Property 'i2s-controller' missing or invalid\n");
-			return (-EINVAL);
-		}
-		dai->dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBS_CFS;
-	}
-	mdelay(50);
-	ret = snd_soc_register_card(&snd_rpi_es9018k2m_dac);
-	if (ret){
-		dev_err(&pdev->dev, "snd_soc_register_card() failed: %d\n", ret);
-	}
-	return ret;
-}
-*/
 
 static int snd_rpi_es9018k2m_dac_probe(struct platform_device *pdev)
 {
